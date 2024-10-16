f07dd4298f8e682483819bac2b5afd62c300dbbc
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2024-10-16 21:22:23 +0300

    commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 //create JS representation from the DOM
 const startText = document.getElementById("startText");
+const paddle1 = document.getElementById("paddle1");
 
 //Games Variable
 let gameRunning = false;
 let keysPressed = {};
+let paddle1Speed = 0;
+let paddle1Y = 150;
 
 document.addEventListener("keydown", startGame);
 document.addEventListener("keydown", handleKeyDown);
-document.addEventListener("keyUp", handleKeyUp);
+document.addEventListener("keyup", handleKeyUp);
 
 // Start Game
 function startGame() {
@@ -34,5 +37,10 @@ function handleKeyUp(e) {
 }
 
 function updatePaddle1() {
-  console.log("keysPressed");
+  if (keysPressed["w"]) {
+    paddle1Speed = -5; // Changed paddleSpeed to paddle1Speed
+  }
+  paddle1Y = paddle1Speed;
+
+  paddle1.style.top = paddle1Y + "px";
 }
